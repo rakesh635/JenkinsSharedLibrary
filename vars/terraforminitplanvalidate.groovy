4a4863f850aaea5e6b7f6703455b592f32cabb17
rakesh635 <rakesh635@gmail.com>
2023-08-29 15:57:25 +0530

    Update terraforminitplanvalidate.groovy

--- vars/terraforminitplanvalidate.groovy
+++ vars/terraforminitplanvalidate.groovy
@@ -1,6 +1,14 @@
-def call() {
-  sh 'terraform init'
-  sh 'terraform fmt'
-  sh 'terraform validate'
-  sh 'terraform plan'
+def call(String init = 'yes', String fmt = 'yes', String validate = 'yes', String plan = 'yes', ) {
+  if(init == 'yes') { 
+    sh 'terraform init'
+  }
+  if(fmt == 'yes') { 
+	sh 'terraform fmt'
+  }
+  if(validate == 'yes') { 
+	sh 'terraform validate'
+  }
+  if(plan == 'yes') { 
+	sh 'terraform plan'
+  }
 }
